7bf8bca912fd59e06a34d160197558e8d464f523
PJ Davis <pj.davis@gmail.com>
2021-08-02 15:37:28 -0400

    Update gemspec file

--- spec/sign_now_sdk_spec.rb
+++ spec/sign_now_sdk_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe SignNowSdk do
   it "has a version number" do
     expect(SignNowSdk::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
